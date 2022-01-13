69eb3bb16904598a5bef7156f421c5381c096daf
Zachary Barton <75874562+ZTBarton@users.noreply.github.com>
2022-01-12 21:04:19 -0700

    Initial check-in of module Mission2

---
+++ Mission2/wwwroot/js/gradeCalculator.js
@@ -0,0 +1,5 @@
+﻿$('#btn').click(function () {
+    let finalGrade = ((parseFloat($('#assignmentGrade').val()) * .55) + (parseFloat($('#groupGrade').val()) * .05) + (parseFloat($('#quizGrade').val()) * .1) + (parseFloat($('#examGrade').val()) * .2) + (parseFloat($('#intexGrade').val()) * .1))
+
+    alert('Final Grade: ' + finalGrade);
+})
