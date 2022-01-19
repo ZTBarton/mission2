564f4f5f197b4cfe43c03add2541c93f0aa868a7
Zachary Barton <75874562+ZTBarton@users.noreply.github.com>
2022-01-19 14:55:46 -0700

    Added Letter

--- Mission2/wwwroot/js/gradeCalculator.js
+++ Mission2/wwwroot/js/gradeCalculator.js
@@ -1,5 +1,43 @@
 ﻿$('#btn').click(function () {
     let finalGrade = ((parseFloat($('#assignmentGrade').val()) * .55) + (parseFloat($('#groupGrade').val()) * .05) + (parseFloat($('#quizGrade').val()) * .1) + (parseFloat($('#examGrade').val()) * .2) + (parseFloat($('#intexGrade').val()) * .1))
+    let letterGrade = '';
 
-    alert('Final Grade: ' + finalGrade);
+    if (finalGrade >= 94) {
+        letterGrade = 'A'
+    }
+    else if (finalGrade >= 90) {
+        letterGrade = 'A-'
+    }
+    else if (finalGrade >= 87) {
+        letterGrade = 'B+'
+    }
+    else if (finalGrade >= 84) {
+        letterGrade = 'B'
+    }
+    else if (finalGrade >= 80) {
+        letterGrade = 'B-'
+    }
+    else if (finalGrade >= 77) {
+        letterGrade = 'C+'
+    }
+    else if (finalGrade >= 74) {
+        letterGrade = 'C'
+    }
+    else if (finalGrade >= 70) {
+        letterGrade = 'C-'
+    }
+    else if (finalGrade >= 67) {
+        letterGrade = 'D+'
+    }
+    else if (finalGrade >= 64) {
+        letterGrade = 'D'
+    }
+    else if (finalGrade >= 60) {
+        letterGrade = 'D-'
+    }
+    else {
+        letterGrade = 'E'
+    }
+
+    alert('Final Grade: ' + finalGrade + '\nLetter Grade: ' + letterGrade);
 })
